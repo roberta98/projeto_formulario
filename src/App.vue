9979e87bbdf5f51de41d43ef9a7e91c5d0ac3872
Dev Muffato <admin@Devs-iMac.local>
2020-08-04 15:09:23 -0300

    Implementação login

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <row :glutter="12" id="app">
+    <column id="container">
+      <newImage :url='default_image'/>
+    </column  >
+    <Login />
+  </row>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Login from './components/Login.vue'
+import Image from './components/newImage.vue'
+import default_image from './assets/texture1.jpg'
+import Vue from 'vue';
+import { Row, Column } from 'vue-grid-responsive'
+
+Vue.component('row', Row);
+Vue.component('column', Column);
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Login,
+    newImage : Image,
+  },
+  data : () => {
+    return { 
+      default_image
+    }
   }
 }
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 1920px;
+  margin: 0 auto;
+}
+#container {
+  display: block;
+  position: relative;
 }
 </style>
---
+++ src/components/newImage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div :xs="3" :md="6" :lg="12"  >
+    <img :src='url' />
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: ['url']
+}
+</script>
+
+<style >
+img{
+  max-width: 100%;
+  height: auto;
+}
+</style>
